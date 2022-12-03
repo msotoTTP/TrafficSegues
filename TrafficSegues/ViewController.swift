d7c8c13eb4e1555e72d71c146ce4128d28f30647
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2022-12-03 14:43:06 -0500

    pass info between screens

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {}
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = titleTextField.text
+    }
+    
 }
 
